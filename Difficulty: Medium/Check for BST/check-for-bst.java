4c289ed02c162050a6d8cfe5fd84b7417d82708b
Akash120806 <akashind5399@gmail.com>
2026-03-09 23:59:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Check for BST/check-for-bst.java
@@ -0,0 +1,16 @@
+class Solution {
+    
+    boolean check(Node root, long min, long max){
+        if(root == null) return true;
+        
+        if(root.data <= min || root.data >= max)
+            return false;
+        
+        return check(root.left, min, root.data) &&
+               check(root.right, root.data, max);
+    }
+    
+    public boolean isBST(Node root) {
+        return check(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+}
